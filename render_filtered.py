b59d63d7c7cb1c37ede744b39aa0fc8238f7e30b
Sami Liedes <sami.liedes@iki.fi>
2022-05-07 22:32:41 +0200

    Render RGB for normal vision.

--- render_filtered.py
+++ render_filtered.py
@@ -37,6 +37,14 @@ def lms_at(lms: FloatArr, lam_: npt.ArrayLike) -> FloatArr:
     return lms[lam - LMS_WAVELEN_MIN]  # type: ignore[no-any-return]
 
 
+# While we simulate arbitrary LMS, for displaying to the user in RGB we still want to use standard LMS
+# (could make this configurable though)
+RGB_TO_STD_LMS: FloatArr = np.array(
+    [lms_at(LMS_STD, WAVELEN_RED), lms_at(LMS_STD, WAVELEN_GREEN), lms_at(LMS_STD, WAVELEN_BLUE)]
+).T
+STD_LMS_TO_RGB = np.linalg.inv(RGB_TO_STD_LMS)  # (3, 3)
+
+
 def sample_lms(lms: FloatArr) -> FloatArr:
     """Sample the given LMS spectrum at points that correspond to the image bands."""
     return lms_at(lms, IMG_WAVELENS)
@@ -63,13 +71,11 @@ def load_lms_image(name: str, my_lms: FloatArr) -> FloatArr:  # (height, width, 
     with chromatic adaptation adjustment corresponding to D65."""
     im = load_multi_image(name)  # (height, width, NUM_IMAGES)
 
-    RGB_TO_LMS: FloatArr = np.array(
-        [lms_at(my_lms, WAVELEN_RED), lms_at(my_lms, WAVELEN_GREEN), lms_at(my_lms, WAVELEN_BLUE)]
-    ).T  # (3, 3)
-
-    LMS_TO_RGB = np.linalg.inv(RGB_TO_LMS)  # (3, 3)
-
     my_lms_sample = sample_lms(my_lms)
+
+    # RGB_TO_LMS: FloatArr = np.array(
+    #     [lms_at(my_lms, WAVELEN_RED), lms_at(my_lms, WAVELEN_GREEN), lms_at(my_lms, WAVELEN_BLUE)]
+    # ).T  # (3, 3)
 
     # We want to set LMS scale factors so that D65 white maps to monitor native white, i.e. RGB 100%, 100%, 100%.
     # This corresponds to chromatic adaptation to D65 illumination.
@@ -77,15 +83,16 @@ def load_lms_image(name: str, my_lms: FloatArr) -> FloatArr:  # (height, width, 
     # (D65 * MULTI_TO_LMS) ⊙ x * LMS_TO_RGB^T = 1.T
     # -->
     # x = (1/(D65 * MULTI_TO_LMS)) ⊙ RGB_TO_LMS * 1
-    LMS_FACTORS = (1 / D65.dot(my_lms_sample)) * RGB_TO_LMS.dot([1, 1, 1])
+    # LMS_FACTORS = (1 / D65.dot(my_lms_sample)) * RGB_TO_LMS.dot([1, 1, 1])
+    LMS_FACTORS = (1 / D65.dot(my_lms_sample)) * RGB_TO_STD_LMS.dot([1, 1, 1])
 
     im_lms = normalize((im * D65).dot(my_lms_sample) * LMS_FACTORS)
-    return normalize(im_lms.dot(LMS_TO_RGB.T))
+    return normalize(im_lms.dot(STD_LMS_TO_RGB.T))
 
 
 def interp_freqs(freqs: FloatArr) -> FloatArr:
     f = interp1d(IMG_WAVELENS, freqs, kind="linear", bounds_error=False, fill_value=1.0)
-    return f(PREC_WAVELENS)
+    return f(PREC_WAVELENS)  # type: ignore[no-any-return]
 
 
 def shift_lms(lms_: FloatArr, l: int, m: int, s: int) -> FloatArr:
